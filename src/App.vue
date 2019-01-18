8c0350babfff0cb5db83fa945342bbac6bf3ce85
Sreng Shuan <srengtrecom@yahoo.com>
2019-01-18 12:13:07 +0700

    images and redirect list get.

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,94 @@
 <template>
   <div id="app">
     <!-- access root props via $root -->
-    <h1 style="text-align: center" v-if="$root.title">{{ $root.title }}</h1>
-    <span>{{ $root.rank }}</span>
-    <at-menu mode="horizontal" :active-name="activeTab" @on-select="switchTab">
-      <at-menu-item name="list">
-        <i class="icon icon-list"></i>
-        List
-      </at-menu-item>
-      <at-menu-item name="edit">
-        <i class="icon icon-plus-square"></i>
-        Add new
-      </at-menu-item>
-    </at-menu>
-    <br>
-    <router-view />
+    <h3>Images list: </h3>
+    <div
+      v-for="(img, k) in images"
+    >
+      <a
+        :href="img"
+        target="_blank"
+        :key="k"
+      >{{ img }}</a>
+    </div>
+
+    <h3>Redirect list: </h3>
+    <div
+      v-for="(rdt, k) in redirects"
+      :key="k"
+    >
+      <a
+        :href="rdt"
+        target="_blank"
+        :key="k"
+      >{{ rdt }}</a>
+    </div>
+    <!--<router-view />-->
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  props: ['title', 'rank'],
+  props: {
+    width: {
+      default: ''
+    },
+    height: {
+      default: ''
+    },
+    type: {
+      default: 'way-parking'
+    },
+    token: {
+      default: ''
+    },
+    tagcount: {
+      default: 5
+    },
+    format: {
+      default: 'jpeg'
+    },
+    rank: {
+      default: 0
+    },
+    eventid: {
+      default: ''
+    }
+  },
   data () {
     return {
-      activeTab: ''
+      dynamic_tag_all: 'https://yesti.me/api/v1/dynamic_tag_all?',
+      images: [],
+      redirects: [],
+      tags: {}
     }
   },
   created () {
-    this.activeTab = this.$route.name
+    const url = this.dynamic_tag_all + 'dimensions=' + this.$root.width + 'x' + this.$root.height + '&type=' + this.$root.type +
+      '&token=' + this.$root.token + '&tagCount=' + this.$root.tagcount + '&format=' + this.$root.format
+    axios
+      .get(url)
+      .then(response => {
+        console.log(response.data)
+        if (response.data.status === 'ok') {
+          this.tags = response.data.tags
+          this.tags.forEach(t => {
+            let imageUrl, redirectUrl
+            imageUrl = t.urls.image.replace('{event_id}', this.$root.eventid)
+            this.images.push(imageUrl)
+            redirectUrl = t.urls.redirect.replace('{event_id}', this.$root.eventid)
+            this.redirects.push(redirectUrl)
+          })
+        }
+        console.log(this.images)
+        console.log(this.redirects)
+      })
   },
   methods: {
-    switchTab (tab) {
-      this.$router.push({ name: tab })
-    }
   },
   watch: {
-    '$route.name' () {
-      this.activeTab = this.$route.name
-    }
   }
 }
 </script>
